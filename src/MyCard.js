6861103f48acd75838eed5a5c85c3cc5ebd8171e
knavin091 <58015353+knavin091@users.noreply.github.com>
2021-12-13 16:26:54 +0530

    api

---
+++ src/MyCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
+
+const MyCard = ({ details }) => {
+  return (
+    <Card>
+      <CardBody className="text-center">
+        <img
+          height="150"
+          width="150"
+          className="rounded-circle img-thumbnail border-danger"
+          src={details.picture?.large}
+        />
+        <CardTitle className="text-primary">
+          <h1>
+            <span className="pr-2">{details.name?.title}</span>
+            <span>{details.name?.first}</span>
+            <span>{details.name?.last}</span>
+          </h1>
+        </CardTitle>
+        <CardText>
+          <FaMapMarkedAlt />
+          {details.location?.city}
+          <p>
+            {details.phone}
+            <br></br>
+            {details.email}
+            <br></br>
+            <br />
+            <span className="text-danger font-weight-bold p-2 border rounded-circle bg-info">
+              {details.dob?.age}
+            </span>
+          </p>
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default MyCard;
